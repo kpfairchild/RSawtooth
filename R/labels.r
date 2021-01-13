6aca0f6ac0602478c23d954bae8dc89879d2f847
kpfairchild <kpfairchild@gmail.com>
2021-01-13 16:07:23 -0700

    added noLabelsParser

--- R/labels.r
+++ R/labels.r
@@ -43,3 +43,97 @@ parseLabelsFile <- function(path)
   class(labelsFileOutput) <- "SSLabelData"
   return(labelsFileOutput)
 }
+
+
+#' Parse Lighthouse Studio Utility Report for label data
+#'
+#' @param utilityFilePath Filepath for Lighthouse Studio Utility Report
+#'
+#' @return Returns list with labels metadata
+#' @export
+#'
+#' @examples noLabelsParser("Excel Report - CBCgolfexercise - HB.xlsx")
+noLabelsParser <- function(utilityFilePath)
+{
+  output <- list()
+
+  summaryPage <- lighthouseUtilityFileSheetReader(utilityFilePath,"Summary",col_names =FALSE)
+
+  NAs <- (1:nrow(summaryPage))[is.na(summaryPage[,1])]
+
+  utilitySectionStartRow     <- grep("Average Utilities",pull(summaryPage[,1]))
+  utilitySectionEndRow       <- min(NAs[NAs > utilitySectionStartRow]) - 1
+  utilityValues <- as.numeric(pull(summaryPage[(utilitySectionStartRow+1):utilitySectionEndRow,2]))
+  levelLabels <- summaryPage[(utilitySectionStartRow+1):utilitySectionEndRow,1] %>% pull()
+  if(length(grep("none",levelLabels[length(levelLabels)],ignore.case = TRUE)))
+  {
+    hasNone <- TRUE
+  } else
+  {
+    hasNone <- FALSE
+  }
+
+  allPairs <- as.matrix(expand.grid(1:length(utilityValues),1:length(utilityValues)))
+  upperPairs <- allPairs[allPairs[,1] < allPairs[,2],]
+
+  cumSumUtilities <- function(x)
+  {
+    sum(utilityValues[seq(x[1],x[2])])
+  }
+
+  pairData <- as_tibble(cbind(1:nrow(upperPairs),
+                              upperPairs,
+                              apply(upperPairs,1,cumSumUtilities)),
+                        .name_repair=~c("i","lowIndex","highIndex","diff"))
+
+  potentialAttrPairs <- pairData[abs(pairData$diff) < 1e-6,]
+
+  nAttrLevels <- NULL
+  coding <- NULL
+  allDone <- FALSE
+  i <- 1
+  while(!allDone)
+  {
+    matchIndex <- which(potentialAttrPairs$lowIndex==i)
+    if(length(matchIndex)>0)
+    {
+      nlevels <- min(potentialAttrPairs[matchIndex,]$highIndex)-i+1
+      nAttrLevels <- c(nAttrLevels,nlevels)
+      i <- i+nlevels
+      coding <- c(coding,"partworth")
+    } else
+    {
+      nAttrLevels <- c(nAttrLevels,1)
+      i <- i+1
+      coding <- c(coding,"linear")
+    }
+    if(i > length(utilityValues)-hasNone)
+    {
+      allDone <- TRUE
+    }
+  }
+
+  nAttr      <- length(nAttrLevels)
+  output$nLevels <- nAttrLevels[1:nAttr]
+
+  output$labelsData <-
+    tibble(attribute = rep(1:nAttr,times=output$nLevels),
+           level = unlist(Vectorize(function(x){1:x})(output$nLevels)),
+           label = ifelse(rep(hasNone,sum(output$nLevels)),levelLabels[-length(levelLabels)],levelLabels)
+    )
+  output$attrCoding <- coding
+
+  importancesSectionStartRow <- grep("Average Importances",pull(summaryPage[,1]))
+  importancesSectionEndRow <- nrow(summaryPage)
+
+  attributeLabels <- summaryPage[(importancesSectionStartRow+1):importancesSectionEndRow,1]
+
+  output$attributeLabels <- attributeLabels
+  output$hasNone <- hasNone
+
+
+  class(output) <- "SSLabelData"
+
+  return(output)
+}
+
